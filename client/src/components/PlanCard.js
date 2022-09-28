d624526366638c9508e816eca6c514031ce8b61b
Cristina Diaz <diazcristina.la@gmail.com>
2022-09-27 17:17:12 -0700

    plan card styling

--- client/src/components/PlanCard.js
+++ client/src/components/PlanCard.js
@@ -1,32 +1,54 @@
-import { Button, Text } from '@chakra-ui/react'
-import React from 'react'
+import {
+	Button,
+	Box,
+	Container,
+	Text,
+	Accordion,
+	AccordionItem,
+	AccordionButton,
+	AccordionPanel,
+	AccordionIcon,
+} from "@chakra-ui/react";
+import React from "react";
 
-function PlanCard({user, plan, setUser }) {
+function PlanCard({ user, plan, setUser }) {
+	function handleAddPlan(e) {
+		let planId = e.target.value;
+		fetch(`/users/${user.id}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ plan_id: planId }),
+		})
+			.then((r) => r.json())
+			.then((updatedUser) => {
+				setUser(updatedUser);
+			});
+	}
+	return (
+		<>
+			<Accordion defaultIndex={[0]} allowMultiple>
+				<AccordionItem>
+					<AccordionButton>
+						<Box flex="1" textAlign="left">
+							<Text fontSize={'lg'}>{plan.name}</Text>{" "}
+						</Box>
+						<AccordionIcon />
+					</AccordionButton>
+					<AccordionPanel pb={4}>
+                        <Text> {plan.description}</Text>
+						<Text> number of runs: {plan.number_of_runs}</Text>
 
-    function handleAddPlan(e){
-
-        let planId = e.target.value
-        fetch(`/users/${user.id}`, {
-            method: 'PATCH',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ plan_id: planId }),
-          })
-            .then((r) => r.json())
-            .then((updatedUser) => {
-
-              setUser(updatedUser);
-            });
-    }
-  return (
-    <div>
-        <Text>{plan.name}</Text>
-        <Text>{plan.number_of_runs}</Text>
-        <Button value={plan.id} onClick={handleAddPlan}> pick plan </Button>
-
-    </div>
-  )
+						<Button value={plan.id} onClick={handleAddPlan} mt={6}>
+							{" "}
+							pick plan{" "}
+						</Button>
+					</AccordionPanel>
+				</AccordionItem>
+			</Accordion>
+		</>
+	);
 }
 
-export default PlanCard
+export default PlanCard;
--- client/src/pages/Plan.js
+++ client/src/pages/Plan.js
@@ -1,20 +1,31 @@
-import { Text } from '@chakra-ui/react'
-import React from 'react'
-import PlanCard from '../components/PlanCard'
+import { Box, Container, Heading, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import PlanCard from "../components/PlanCard";
 
-function Plan({user, setUser, plans}) {
+function Plan({ user, setUser, plans }) {
 
-console.log(user.plan_id)
 
-    const planCards = plans.map( (plan) => <PlanCard user={user} setUser={setUser} plan={plan} key={plan.id} />)
-    const currentPlan = plans.find( plan => plan.id === user.plan_id)
-    console.log(currentPlan)
-  return (
-    <>{user.plan_id ? <Text> current plan: {currentPlan.name} </Text> : null}
-    
-        {planCards}    
-    </>
-  )
+	const planCards = plans.map((plan) => (
+		<PlanCard user={user} setUser={setUser} plan={plan} key={plan.id} />
+	));
+	const currentPlan = plans.find((plan) => plan.id === user.plan_id);
+
+	return (
+		<>
+			{user.plan_id ? (
+				<>
+					<Heading size='lg' my={5} ml={3}> current plan: </Heading>
+					<Container centerContent>
+						<Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' background-image={'https://i.ibb.co/F58Ydt0/5KRUN.jpg' } >
+							<Image src="https://i.ibb.co/F58Ydt0/5KRUN.jpg" />
+						</Box>
+					</Container>
+				</>
+			) : null}
+            <Heading size={'lg'} my={5} ml={3}> pick a new plan</Heading>
+			{planCards}
+		</>
+	);
 }
 
-export default Plan
+export default Plan;
